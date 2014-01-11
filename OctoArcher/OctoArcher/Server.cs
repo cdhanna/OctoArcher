f3f154895b67e4300cc4cc721d7c01a12d6e36f7
Greg Daneault <gregdaneault@gmail.com>
2014-01-11 03:38:07 -0500

    Added basic server

---
+++ OctoArcher/OctoArcher/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace OctoArcher
+{
+    class Server
+    {
+        static TcpListener listener;
+
+        public static void Main()
+        {
+            listener = new TcpListener(1025);
+            listener.Start();
+
+            while (true)
+            {
+                Thread t = new Thread(new ThreadStart(Handler));
+                t.Start();
+            }
+        }
+
+        public static void Handler()
+        {
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Received connection from " + socket.RemoteEndPoint);
+
+            try
+            {
+                Stream s = new NetworkStream(socket);
+                StreamReader reader = new StreamReader(s);
+                StreamWriter writer = new StreamWriter(s);
+
+                writer.AutoFlush = true;
+                writer.WriteLine("Hello from 192.168.1.11");
+
+                s.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
